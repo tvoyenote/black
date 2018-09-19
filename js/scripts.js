0f257ec892ed30b3b8149c9b843814787c345241
Nata <missesunme@gmail.com>
2018-09-19 15:23:36 +0300

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,1 @@
-var menuElem = document.getElementById('nav');
-var titleElem = menuElem.querySelector('.title');
-
-titleElem.onclick = function() {
-  menuElem.classList.toggle('open');
-};
+$("link").hide();
