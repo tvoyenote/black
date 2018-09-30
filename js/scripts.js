99332f09a00550c4c75e4db62f8c16c3f1f189e0
Nata <missesunme@gmail.com>
2018-09-30 15:11:26 +0300

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,26 @@ $(".link").click(function(){
     $(".wrapper").slideToggle();
 });
 
-// $(".link").click(function () {
-//     $(this).show(".menu-item", { direction: "left" }, 1000);
-// });
-
 $(".btn.btn-outline-secondary").click(function(){
     $(".second__text").slideToggle();
+});
+
+$('.js-shuffle').shuffleLetters();
+
+$('#circle').circleProgress({
+    value: 0.9
+}).on('circle-animation-progress', function(event, progress) {
+  $(this).find('strong').html(Math.round(90 * progress) + '<i>%</i>');
+});
+
+$('#circle1').circleProgress({
+    value: 0.75
+}).on('circle-animation-progress', function(event, progress) {
+  $(this).find('strong').html(Math.round(75 * progress) + '<i>%</i>');
+});
+
+$('#circle2').circleProgress({
+    value: 0.8
+}).on('circle-animation-progress', function(event, progress) {
+  $(this).find('strong').html(Math.round(80 * progress) + '<i>%</i>');
 });
